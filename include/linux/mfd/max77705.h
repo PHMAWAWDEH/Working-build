750b3c7a86a125a66b7582bc7767b6d1abd05986
sanded <36462714+Hussein995@users.noreply.github.com>
2024-08-24 10:33:38 +0300

    Update max77705.h

--- include/linux/mfd/max77705.h
+++ include/linux/mfd/max77705.h
@@ -33,33 +33,12 @@
 #define M2SH(m) ((m) & 0x0F ? ((m) & 0x03 ? ((m) & 0x01 ? 0 : 1) : ((m) & 0x04 ? 2 : 3)) : \
 		((m) & 0x30 ? ((m) & 0x10 ? 4 : 5) : ((m) & 0x40 ? 6 : 7)))
 
-#if defined(CONFIG_MOTOR_DRV_MAX77705)
-	struct max77705_haptic_pdata {
-		u16 max_timeout;
-		u16 duty;
-		u16 period;
-		u16 reg2;
-		bool overdrive_state;
-		int gpio;
-		const char *regulator_name;
-		unsigned int pwm_id;
-		const char *vib_type;
-
-		/* for multi-frequency */
-		int multi_frequency;
-		int freq_num;
-		u32 *multi_freq_period;
-		int normal_ratio;
-		int overdrive_ratio;
-		int high_temp_ratio;
-		int temperature;
-	};
-#endif
-
+#if defined(CONFIG_MAX77705_VIBRATOR)
 struct max77705_vibrator_pdata {
 	int gpio;
 	char *regulator_name;
 	struct pwm_device *pwm;
+	unsigned int pwm_id;
 	const char *motor_type;
 
 	int freq;
@@ -69,7 +48,18 @@ struct max77705_vibrator_pdata {
 	u32 *ratio_array; /* not used now */
 	int normal_ratio;
 	int overdrive_ratio;
+	int high_temp_ratio;
+	int high_temp_ref;
+	int fold_open_ratio;
+	int fold_close_ratio;
+#if defined(CONFIG_SEC_VIBRATOR)
+	bool calibration;
+	int steps;
+	int *intensities;
+	int *haptic_intensities;
+#endif
 };
+#endif
 
 struct max77705_regulator_data {
 	int id;
@@ -83,16 +73,14 @@ struct max77705_platform_data {
 	int irq_gpio;
 	bool wakeup;
 	bool blocking_waterevent;
-	bool extra_fw_enable;
 	int wpc_en;
 	int fw_product_id;
 	struct muic_platform_data *muic_pdata;
 
 	int num_regulators;
 	struct max77705_regulator_data *regulators;
+#if defined(CONFIG_MAX77705_VIBRATOR)
 	struct max77705_vibrator_pdata *vibrator_data;
-#if defined(CONFIG_MOTOR_DRV_MAX77705)
-	struct max77705_haptic_pdata *haptic_data;
 #endif
 	struct mfd_cell *sub_devices;
 	int num_subdevs;
@@ -105,5 +93,3 @@ struct max77705 {
 };
 
 #endif /* __MAX77705_H__ */
-
-
